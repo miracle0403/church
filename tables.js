53a59a7398db2a551f7d5e66b953b95221d45308
Miracle Lawrence <mify1@yahoo.com>
2019-03-11 19:48:17 +0100

    gh

--- tables.js
+++ tables.js
@@ -1,4 +1,4 @@
-var db = require( './db.js' );
+/*var db = require( './db.js' );
 
 //create events table
 db.query( 'CREATE TABLE IF NOT EXISTS events ( id INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL, event_name varchar( 255 ) NOT NULL, event_date DATETIME NOT NULL, venue varchar( 255 ) NOT NULL, image varchar ( 255 ) NOT NULL, description text NOT NULL )', function( err, results ){
@@ -11,3 +11,14 @@ db.query( 'CREATE TABLE IF NOT EXISTS attendance (event_name varchar( 255 ) NOT 
 	if( err ) throw err;
 	console.log( 'attendance table created' );
 });
+
+//create events table
+db.query( 'CREATE TABLE IF NOT EXISTS messages ( id INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL, event_date DATETIME NOT NULL, date_preached DATETIME NOT NULL, video varchar ( 255 ) NOT NULL, description text NOT NULL )', function( err, results ){
+	if( err ) throw err;
+	console.log( 'messages table created' );
+});
+
+db.query( 'CREATE TABLE IF NOT EXISTS news (topic varchar( 255 ) NOT NULL, description varchar ( 255 ) NOT NULL, image varchar( 255 ) null, date_entered DATETIME DEFAULT CURRENT_TIMESTAMP)', function( err, results ){
+	if( err ) throw err;
+	console.log( 'news table created' );
+});*/
